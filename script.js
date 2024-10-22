f86a30a759a569d5b9f62933c36b444571950ec1
THartigan <57837418+THartigan@users.noreply.github.com>
2024-10-22 10:53:00 +0100

    improved header

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 window.addEventListener('scroll', function () {
   const header = document.querySelector('header');
-  if (window.scrollY > 50) {
+  if (window.scrollY > 0) {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
